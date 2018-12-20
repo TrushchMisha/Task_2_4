6ea0d8c342ca1cef7e5123e802b19c003d423fb6
TrushchMisha <43968047+TrushchMisha@users.noreply.github.com>
2018-12-20 21:39:13 +0200

    task

--- app/src/main/java/com/example/compi3/myapplication/MainActivity.java
+++ app/src/main/java/com/example/compi3/myapplication/MainActivity.java
@@ -3,15 +3,37 @@ package com.example.compi3.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText txt = findViewById(R.id.root);
+        Button myButton = findViewById(R.id.btn);
+
+        myButton.setOnClickListener(new OnClickListener(){
+            public void onClick(View v) {
+                CharSequence s = txt.getText().toString();
+                Toast toast = Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT);
+                toast.show();
+
+            }
+
+        });
+
+
     }
 
     @Override
@@ -42,5 +64,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy();");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
     }
 }
